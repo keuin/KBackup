e372f3bf588e9fbb6493b1c4b8abaa2b0a24af00
keuin <keuinx@gmail.com>
2020-02-28 22:55:48 +0800

    First commit, just for some initial files.

---
+++ KBackup/src/com/keuin/kbackup/KBackup.java
@@ -0,0 +1,16 @@
+package com.keuin.kbackup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KBackup extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    	
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    	
+    }
+}
