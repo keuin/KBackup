e96c24d8ec38391a2257291a65ccad6c6f643afe
keuin <keuinx@gmail.com>
2020-02-29 13:50:22 +0800

    basic backupWorld logic, using Zip to make a primitive archive

--- src/com/keuin/kbackup/KBackup.java
+++ src/com/keuin/kbackup/KBackup.java
@@ -1,16 +1,77 @@
 package com.keuin.kbackup;
 
+import java.io.IOException;
+
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class KBackup extends JavaPlugin {
+public final class KBackup extends JavaPlugin implements Listener {
+	
+	private String backupDirectory;
+	FileConfiguration config;
+	PluginLogger logger;
+	
     // Fired when plugin is first enabled
     @Override
     public void onEnable() {
+    	// Initialize plugin logger
+    	logger = new PluginLogger(this);
+    	// Initialize config file
+    	config = this.getConfig();
+    	config.addDefault("backupDirectory", "./backups"); // default backups reside in the subdir "backups" of your server root dir
+    	this.backupDirectory = config.getString("backupDirectoory");
     	
+    	config.options().copyDefaults(true);
+    	saveConfig();
+    	
+    	logger.info("KBackup loaded successfully.");
     }
+    
+//    @EventHandler
+//    public void onPlayerJoin(PlayerJoinEvent event) {
+//    	
+//    }
+    
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
-    	
+    	// Save all config
+    	config.options().copyDefaults(true);
+    	saveConfig();
     }
+    
+    private boolean backupWorld(World world) {
+    	logger.info(String.format("Backing up world %s ...", world.getName()));
+    	boolean previousAutoSaveSwitch = world.isAutoSave(); // if this world is set to be auto saved
+    	
+    	try {
+        	logger.info("Saving world data to disk...");
+        	world.save();
+        	logger.info("World saved.");
+        	if(previousAutoSaveSwitch) {
+        		logger.info("Disabling auto-save temporarily to prevent data inconsistency.");
+        		world.setAutoSave(false); // Disable auto saving temporarily to prevent world data inconsistency
+        	}
+        	
+        	logger.info(String.format("Compressing world data for world %s. Please DO NOT save manually during backup process.",world.getName()));
+        	// TODO: Do some zip operation
+        	ZipUtil.zip(world.getWorldFolder(), zipPath, zipFileName);
+    	} catch(ZipUtilException exception) {
+    		
+    	} catch(IOException exception) {
+    		
+    	} finally {
+    		
+    	}
+    	
+    	if(previousAutoSaveSwitch)
+    		world.setAutoSave(true);
+    	return true;
+    }
+    
+    
 }
---
+++ src/com/keuin/kbackup/ZipUtilException.java
@@ -0,0 +1,8 @@
+package com.keuin.kbackup;
+
+public class ZipUtilException extends Exception {
+	public ZipUtilException() {}
+	public ZipUtilException(String gripe) {
+		super(gripe);
+	}
+}
